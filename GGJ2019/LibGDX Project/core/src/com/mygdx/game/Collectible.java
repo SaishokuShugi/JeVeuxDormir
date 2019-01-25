c84062d9784f99e8edbe78aa903421406d9b73b3
julkoh <julkoh@hotmail.fr>
2019-01-25 22:21:34 +0100

    Ajout Collectible
    Signed-off-by: julkoh <julkoh@hotmail.fr>

--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/Collectible.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/Collectible.java
@@ -3,16 +3,16 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class Obstacle {
+public class Collectible {
     private Texture image;
     private Body body;
 
-    public Obstacle(String image, Body body) {
+    public Collectible(String image, Body body) {
         this.image = new Texture(image);
         this.body = body;
     }
 
-    public Obstacle(String image, BodyDef.BodyType bodyType, int x, int y, PolygonShape shape, float friction, float density, float restitution) {
+    public Collectible(String image, BodyDef.BodyType bodyType, int x, int y, PolygonShape shape, float friction, float density, float restitution) {
         this.image = new Texture(image);
         BodyDef bd = new BodyDef();
         bd.type = bodyType;
