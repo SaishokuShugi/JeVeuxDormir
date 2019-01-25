3b272f9ecb99841b680495b65c9c2cc7267968e1
julkoh <julkoh@hotmail.fr>
2019-01-25 23:46:11 +0100

    Vanish
    Signed-off-by: julkoh <julkoh@hotmail.fr>

--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/Block.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/Block.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 
 public class Block extends Interactible {
 
-    public Block(String image, int frame_cols, int frame_rows, int x, int y, float friction, float density, float restitution) {
+    public Block(String image, int frame_cols, int frame_rows, int x, int y, float density) {
         super(image, frame_cols, frame_rows, BodyDef.BodyType.StaticBody, x, y, 0f, density, 0f);
     }
 
