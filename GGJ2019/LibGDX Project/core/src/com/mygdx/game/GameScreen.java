639bc516847a9f31c49e316a93d0a169c9e1ca57
stduhpf <stephduh@live.fr>
2019-01-26 13:59:58 +0100

    euh

--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/GameScreen.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/GameScreen.java
@@ -5,14 +5,18 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.ArrayList;
 
@@ -27,12 +31,12 @@ public class GameScreen implements Screen {
 	public static float scale_factor;
 
 	public static World world;
-
+	float time =0;
 	Controller cont;
 	Box2DDebugRenderer debugRenderer;
 	public ArrayList<Interactible> blocks = new ArrayList<Interactible>();
 	
-
+	ShaderProgram shader;
 	
 	void generateMap() {
 		for(float i = 0;i<15;blocks.add(new Block("Sol.png", 2, 2, i++, 0f, 0, 0, 0)));
@@ -41,7 +45,28 @@ public class GameScreen implements Screen {
 		blocks.add(new Block("Armoire.png", 1, 1, 8f, 1f, 0, 0, 0));
 		blocks.add(new Block("Table.png", 1, 1, 4f, 1f, 0, 0, 0));
 		blocks.add(new Movable("Commode.png", 1, 1, 4f, 2f, 0, 0, 0));
-		perso = new Personnage(10, 10, 0f, 2f, 1, 10, 0, Personnage.loadAnim("Personnage/idle.png", 4, 4, .1f));
+		perso = new Personnage(10, 10, 0f, 2f, 1, 10, 0);
+	}
+	
+	
+	void  loadShader() {
+		String vertexShader = "attribute vec4 a_position;    \n" + 
+                "attribute vec4 a_color;\n" +
+                "attribute vec2 a_texCoord0;\n" + 
+                "uniform mat4 u_projTrans;\n" + 
+                "varying vec2 v_texCoords;" + 
+                "void main()                  \n" + 
+                "{                            \n" +
+                "   v_texCoords = a_texCoord0; \n" + 
+                "   gl_Position =  u_projTrans * a_position;  \n"      + 
+                "}                            \n" ;
+		String fragmentShader ="varying vec2 v_texCoords;\n" + 
+                  "uniform sampler2D u_texture;\n" + 
+                  "void main()                                  \n" + 
+                  "{                                            \n" + 
+                  "  gl_FragColor = vec4(1,0,0,1) * texture2D(u_texture, v_texCoords);\n" +
+                  "}";
+		shader = new ShaderProgram(vertexShader, fragmentShader);
 	}
 	
 	
@@ -60,9 +85,10 @@ public class GameScreen implements Screen {
 		if (!Controllers.getControllers().isEmpty())
 			cont = Controllers.getControllers().get(0);
 		Box2D.init();
-		world = new World(new Vector2(0, -9.81f * scale_factor), true);
+		world = new World(new Vector2(0, -9.81f * scale_factor*32), true);
 		debugRenderer = new Box2DDebugRenderer();
 		generateMap();
+		loadShader();
 	}
 
 	@Override
@@ -79,16 +105,37 @@ public class GameScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		debugRenderer.render(world, camera.combined);
+		byte[] pixels = ScreenUtils.getFrameBufferPixels(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), true);
+
+		// this loop makes sure the whole screenshot is opaque and looks exactly like what the user is seeing
+		for(int i = 4; i < pixels.length; i += 4) {
+		    pixels[i - 1] = (byte) 255;
+		}
+
+		Pixmap pixmap = new Pixmap(Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), Pixmap.Format.RGBA8888);
+		Texture Frame = new Texture(pixmap);
+		shader.begin();
+
+		Gdx.graphics.getGL20().glActiveTexture(GL20.GL_TEXTURE0);
+	     Frame.bind(0);
+	     shader.setUniformi("u_texture", 0);
+		
 		batch.begin();
 		batch.draw(new Texture("Background.png"),0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
 		for (Interactible block : blocks) {
 			img = block.getImages()[block.tile];
 			batch.draw(img, block.getBodyXToImage(), block.getBodyYToImage(), img.getRegionWidth() * scale_factor,
 					img.getRegionHeight() * scale_factor);
-			world.step(Math.min(.015f, deltat), 6, 2);
 		}
+		perso.setAnimTime(time+=deltat);
+		batch.draw(perso.getCurrentFrame(), perso.getBody().getPosition().x, perso.getBody().getPosition().y);
 
 		batch.end();
+
+	    shader.end();
+		world.step(Math.min(.015f, deltat), 6, 2);
+
+
 	}
 
 	@Override
