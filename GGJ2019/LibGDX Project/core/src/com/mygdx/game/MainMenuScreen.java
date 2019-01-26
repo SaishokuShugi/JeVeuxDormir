4782c52967860cfe47540e32322316b653040dcc
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2019-01-26 06:04:22 +0100

    Menu v1.fgbfgbed

--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/MainMenuScreen.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/MainMenuScreen.java
@@ -1,34 +1,39 @@
 package com.mygdx.game;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-public class MainMenuScreen implements Screen {
+public class MainMenuScreen implements Screen
+{
 
 	final MyGdxGame game;
 	OrthographicCamera camera;
 	FreeTypeFontGenerator ftfg = new FreeTypeFontGenerator(Gdx.files.internal("SleeplessCity.ttf"));
-	BitmapFont createFont(FreeTypeFontGenerator ftfg, int taille, boolean inverseCouleur , boolean border)
+
+	BitmapFont createFont(FreeTypeFontGenerator ftfg, int taille, boolean inverseCouleur, boolean border)
 	{
 		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
 		parameter.size = taille;
-		
+
 		if (inverseCouleur)
 		{
 			parameter.borderColor = Color.WHITE;
 			parameter.color = Color.BLACK;
-		}
-		else 
+		} else
 		{
 			parameter.borderColor = Color.BLACK;
 			parameter.color = Color.WHITE;
@@ -36,30 +41,40 @@ public class MainMenuScreen implements Screen {
 		if (border)
 		{
 			parameter.borderWidth = 3f;
-		}
-		else
+		} else
 		{
 			parameter.borderWidth = 0f;
 		}
-		
-	    return ftfg.generateFont(parameter);
+
+		return ftfg.generateFont(parameter);
 	}
-	
+
 	BitmapFont TitreFont = createFont(ftfg, 55, false, true);
 	BitmapFont SousTitreFont = createFont(ftfg, 15, false, true);
 	BitmapFont ButtonFont1 = createFont(ftfg, 30, true, true);
 	BitmapFont ButtonFont2 = createFont(ftfg, 30, false, false);
-	
+
 	Texture LitImage = new Texture(Gdx.files.internal("Lit.png"));
 	Texture ButtonImage = new Texture(Gdx.files.internal("Commode.png"));
-//	Animation<TextureRegion> = new Animation<TextureRegion>();
-	Vector3 MousePos = new Vector3(0f, 0f, 0f);
-	Vector2 PersoPos = new Vector2(-320,0);
-	
-	Boolean onPlay, 
-			onOption;
+	Texture SolTextureMap = new Texture(Gdx.files.internal("Sol.png"));
+	Texture FondImage = new Texture(Gdx.files.internal("Background.png"));
+	TextureRegion[][] SolImages = TextureRegion.split(SolTextureMap, 64, 32);
+	Texture PersoRunTextureMap = new Texture(Gdx.files.internal("Personnage/run.png"));
+	Animation<TextureRegion> RunAnim1 = Personnage.loadAnim("Personnage/run.png", 4, 2, 0.25f);
+	Animation<TextureRegion> RunAnim2 = Personnage.loadAnim("Personnage/run.png", 4, 2, 0.5f);
+	Animation<TextureRegion> RunAnim3 = Personnage.loadAnim("Personnage/run.png", 4, 2, 0.75f);
+	Animation<TextureRegion> RunAnim4 = Personnage.loadAnim("Personnage/run.png", 4, 2, 1f);
 
-	public MainMenuScreen(final MyGdxGame game) {
+	
+	Vector3 MousePos = new Vector3(0f, 0f, 0f);
+	Vector2 PersoPos = new Vector2(-320, 10);
+
+	Boolean onPlay, onOption;
+	
+	float stateTime = 0f;
+
+	public MainMenuScreen(final MyGdxGame game)
+	{
 		this.game = game;
 
 		camera = new OrthographicCamera();
@@ -68,16 +83,18 @@ public class MainMenuScreen implements Screen {
 	}
 
 	@Override
-	public void show() {
+	public void show()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void render(float delta) {
+	public void render(float delta)
+	{
 		// TODO Auto-generated method stub
 		MousePos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-		
+
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -86,43 +103,56 @@ public class MainMenuScreen implements Screen {
 		game.batch.setProjectionMatrix(camera.combined);
 
 		game.batch.begin();
+		game.batch.draw(FondImage, 0, 0, 1280, 720);
 		TitreFont.draw(game.batch, "Je Veux Dormir ", 120, 700);
 		SousTitreFont.draw(game.batch, "A Vegan Product", 140, 625);
 		game.batch.draw(ButtonImage, 0, 255, 320, 320);
-		game.batch.draw(LitImage, 1280-320, 0, 320,320);
 		
 		
-		if(MousePos.x> 50 && MousePos.x < 270 && MousePos.y> 390 && MousePos.y< 490)
+		int i = 0;
+		while (i<1280)
+		{
+			game.batch.draw(SolImages[0][0], i*128,-30, 128, 64);
+			i++;
+		}
+		game.batch.draw(LitImage, 1280 - 320, 10, 320, 320);
+
+		if (MousePos.x > 50 && MousePos.x < 270 && MousePos.y > 390 && MousePos.y < 490)
 		{
 			ButtonFont1.draw(game.batch, "Play", 127, 450);
 			onPlay = true;
-		}
-		else 
+		} else
 		{
 			ButtonFont2.draw(game.batch, "Play", 127, 450);
 			onPlay = false;
 		}
-		
-		if(MousePos.x> 50 && MousePos.x < 270 && MousePos.y> 290 && MousePos.y< 390)
+
+		if (MousePos.x > 50 && MousePos.x < 270 && MousePos.y > 290 && MousePos.y < 390)
 		{
 			ButtonFont1.draw(game.batch, "Option", 105, 350);
 			onOption = true;
-		}
-		else 
+		} else
 		{
 			ButtonFont2.draw(game.batch, "Option", 105, 350);
 			onOption = false;
 		}
+
+		stateTime += Gdx.graphics.getDeltaTime();
+		
+		TextureRegion currentFrame = RunAnim1.getKeyFrame(stateTime, true);
+		game.batch.draw(currentFrame, PersoPos.x, PersoPos.y, 210, 330);
 		
 		game.batch.end();
+		
+		PersoPos.x += 2.5f;
 
-		if (Gdx.input.isTouched()) {
+		if (Gdx.input.isTouched())
+		{
 			if (onPlay)
 			{
 				game.setScreen(new GameScreen(game));
 				dispose();
-			}
-			else if (onOption)
+			} else if (onOption)
 			{
 				game.setScreen(new OptionScreen(game));
 				dispose();
@@ -131,40 +161,43 @@ public class MainMenuScreen implements Screen {
 	}
 
 	@Override
-	public void resize(int width, int height) {
+	public void resize(int width, int height)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void pause() {
+	public void pause()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void resume() {
+	public void resume()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void hide() {
+	public void hide()
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void dispose() {
+	public void dispose()
+	{
 		// TODO Auto-generated method stub
-		//game.batch.dispose();
+		// game.batch.dispose();
 		TitreFont.dispose();
 		SousTitreFont.dispose();
 		ButtonFont1.dispose();
 		ButtonFont2.dispose();
-		
-		
-	}
 
+	}
 
 }
