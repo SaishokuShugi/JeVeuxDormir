b3192704f1bb15493354e58e38e7883063462d3c
GlobalFistingLeague <bastien@Wooden-Arch.localdomain>
2019-01-26 03:40:58 +0100

    Clickable menu

---
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/OptionScreen.java
@@ -0,0 +1,72 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class OptionScreen implements Screen
+{
+	final MyGdxGame game;
+	private OrthographicCamera camera;
+	SpriteBatch batch;
+	
+	public OptionScreen (final MyGdxGame game) {
+		this.game = game;
+		// create the camera and the SpriteBatch
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,Gdx.graphics.getWidth() , Gdx.graphics.getHeight());
+	}
+
+	@Override
+	public void show()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
