3cce635aa667d2677cf944182b553f844df5cea7
julkoh <julkoh@hotmail.fr>
2019-01-26 02:25:48 +0100

    Classes des Interactibles
    Signed-off-by: julkoh <julkoh@hotmail.fr>

---
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/Tombant.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+public class Tombant extends Movable {
+
+    public Tombant(String image, int frame_cols, int frame_rows, int x, int y, float friction, float density, float restitution) {
+        super(image, frame_cols, frame_rows, x, y, friction, density, restitution);
+    }
+
+    @Override
+    public void action() {
+
+    }
+}
