686a36f912d33dc5daf7f649c3a07bd5a9cf1697
julkoh <julkoh@hotmail.fr>
2019-01-26 23:22:58 +0100

    classes avec sensor
    Signed-off-by: julkoh <julkoh@hotmail.fr>

--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/Collectible.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/Collectible.java
@@ -4,8 +4,8 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 
 public class Collectible extends Interactible {
 
-    public Collectible(String image, int frame_cols, int frame_rows, int nbFrames, float x, float y, float friction, float density, float restitution) {
-        super(image, frame_cols, frame_rows, nbFrames, BodyDef.BodyType.DynamicBody, x, y, friction, density, restitution);
+    public Collectible(String image, int frame_cols, int frame_rows, int nbFrames, float x, float y, float friction, float density, float restitution, boolean sensor) {
+        super(image, frame_cols, frame_rows, nbFrames, BodyDef.BodyType.DynamicBody, x, y, friction, density, restitution, sensor);
     }
 
     public void action(Personnage perso, float staminaFactor, float froidFactor) {
--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/Tombant.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/Tombant.java
@@ -2,8 +2,8 @@ package com.mygdx.game;
 
 public class Tombant extends Movable {
 
-    public Tombant(String image, int frame_cols, int frame_rows, int nbFrames, float x, float y, float friction, float density, float restitution) {
-        super(image, frame_cols, frame_rows, nbFrames, x, y, friction, density, restitution);
+    public Tombant(String image, int frame_cols, int frame_rows, int nbFrames, float x, float y, float friction, float density, float restitution, boolean sensor) {
+        super(image, frame_cols, frame_rows, nbFrames, x, y, friction, density, restitution, sensor);
     }
 
     public void action() {
