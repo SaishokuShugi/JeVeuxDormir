44a42e1410f031bc7c94b2c2d9ddffe547d35a6a
julkoh <julkoh@hotmail.fr>
2019-01-26 12:35:02 +0100

    C'est l'heure de se réveiller
    Signed-off-by: julkoh <julkoh@hotmail.fr>

--- GGJ2019/LibGDX Project/core/src/com/mygdx/game/GameScreen.java
+++ GGJ2019/LibGDX Project/core/src/com/mygdx/game/GameScreen.java
@@ -1,14 +1,7 @@
 package com.mygdx.game;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.GL20;
@@ -16,15 +9,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.ArrayList;
 
 public class GameScreen implements Screen {
 	final MyGdxGame game;
@@ -45,10 +35,10 @@ public class GameScreen implements Screen {
 	void generateMap() {
 		for(float i = 0;i<15;blocks.add(new Block("Sol.png", 2, 2, i++, 0f, 0, 0, 0)));
 		blocks.add(new Movable("Chaise.png", 1, 1, 1f, 2f, 0, 0, .5f));
-		blocks.add(new Block("lit.png", 1, 1, 13f, 1f, 0, 0, 0));
-		blocks.add(new Block("armoire.png", 1, 1, 8f, 1f, 0, 0, 0));
-		blocks.add(new Block("table.png", 1, 1, 4f, 1f, 0, 0, 0));
-		blocks.add(new Movable("commode.png", 1, 1, 4f, 2f, 0, 0, 0));
+		blocks.add(new Block("Lit.png", 1, 1, 13f, 1f, 0, 0, 0));
+		blocks.add(new Block("Armoire.png", 1, 1, 8f, 1f, 0, 0, 0));
+		blocks.add(new Block("Table.png", 1, 1, 4f, 1f, 0, 0, 0));
+		blocks.add(new Movable("Commode.png", 1, 1, 4f, 2f, 0, 0, 0));
 	}
 	
 	
